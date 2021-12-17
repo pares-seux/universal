5e611717077991aebe6a7e3e3981e9f0ad226550
pares-seux <paresseux.992@gmail.com>
2021-12-17 08:32:53 +0400

    Fix: edited mistakes

--- js/main.js
+++ js/main.js
@@ -3,15 +3,17 @@ menuButton.addEventListener('click', function() {
   document.querySelector('.navbar-menu').classList.toggle('navbar-menu--visible');
 }, {passive: true})
 
-const research = document.getElementsByClclass('research__parallax')[0];
+const research = document.getElementsByClassName('research__parallax')[0];
+
 function moveBg(e){
   var researchHeight = research.getBoundingClientRect().height;
-  var heightScroll = window.innerHeight + researchHeight - 40;
+  var heightScroll = window.innerHeight + researchHeight - 60;
   var researchTop = research.getBoundingClientRect().top;
-  var blockPosition = researchTop + researchHeight - 40;
+  var blockPosition = researchTop + researchHeight - 60;
 
   if ((researchTop-window.innerHeight<=0) && (researchTop+researchHeight>=0)) {
-    research.style.backgroundPositionY = ((60-0.6*100) + (blockPosition/heightScroll)*100*0.6) + '%'; }
+    research.style.backgroundPositionY = ((80-0.8*100) + (blockPosition/heightScroll)*100*0.8) + '%'; 
+  }
 
 };
 
