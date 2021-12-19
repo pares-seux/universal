abe13d245792dce4fa6aaf42581206ccc5f27830
pares-seux <paresseux.992@gmail.com>
2021-12-19 18:19:31 +0400

    Create: finished first page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var menuButton = document.querySelector('.menu-button');
+$(document).ready(function () {
+  var menuButton = document.querySelector('.menu-button');
 menuButton.addEventListener('click', function() {
   document.querySelector('.navbar-menu').classList.toggle('navbar-menu--visible');
 }, {passive: true})
@@ -55,5 +56,71 @@ const reviewSlider = new Swiper('.advice-slider', {
   pagination: {
     el: '.advice-slider__pagination',
     type: 'bullets'
+  },
+  autoplay: {
+    delay: 4000,
   }
+});
+
+var modalButton = $('[data-toggle=modal]');
+  modalButton.on('click', openModal);
+  var closeModalButton = $('.modal__close');
+  closeModalButton.on('click', closeModal);
+  var modalWindow = $('.modal__overlay');
+  modalWindow.on('click', function(event) {
+    closeModal(event);
+  });
+
+  $(document).keyup(function(event) {
+     if (event.key === "Escape") { 
+       closeModal(event);
+    }
+  });
+
+  function openModal() {
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.addClass('modal__overlay--visible');
+    modalDialog.addClass('modal__dialog--visible');
+  };
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  };
+
+  //обработка форм
+  $(".form").each( function() {
+    if($(this).hasClass('footer-subscription__form')) {
+      $(this).validate({
+        errorLabelContainer: $('.footer-subscription__label'),
+        messages: {
+          email: {
+            required: 'Обязательное поле*',
+            email: 'Пожалуйста, введите корректный email-адрес'
+          },
+        },
+    });
+    };
+    $(this).validate({
+      messages: {
+        name: {
+          required: 'Обязательное поле*',
+          name: 'Имя не должно быть короче 2х'
+        },
+        phone: {
+          required: 'Обязательное поле*',
+          minlength:  'Пожалуйста, введите корректный номер +7 (XXX) XXX-XX-XX'
+        },
+        email: {
+          required: 'Обязательное поле*',
+          email: 'Пожалуйста, введите корректный email-адрес'
+        },
+      },
+    });
+  });
+
 });
